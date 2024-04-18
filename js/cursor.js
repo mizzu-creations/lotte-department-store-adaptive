12ed65e4dd6a97be4afa971d2255c0e69f16eaf2
mizzu-creations <mizzu.creations@gmail.com>
2024-04-18 18:57:07 +0900

    Feat : 커스텀 마우스 커서 제작

---
+++ js/cursor.js
@@ -0,0 +1,90 @@
+const cursor = document.querySelector(".cursor");
+const cursorPrev = document.querySelector(".cursor-slide.prev");
+const cursorNext = document.querySelector(".cursor-slide.next");
+const card = document.querySelector(".sample");
+const list = document.querySelector(".sample2");
+let isLoading = true;
+let isSlideOpen = false;
+
+function setCursorPosition(left, top) {
+  gsap.to(cursor, 0.4, {
+    left: `${left}px`,
+    top: `${top}px`,
+  });
+  if (isSlideOpen) {
+    gsap.to(cursorPrev, 0.4, {
+      left: `${left - 70}px`,
+      top: `${top - 10}px`,
+    });
+    gsap.to(cursorNext, 0.4, {
+      left: `${left + 70}px`,
+      top: `${top - 10}px`,
+    });
+  } else {
+    gsap.to(cursorPrev, 0.4, {
+      left: `${left - 60}px`,
+      top: `${top - 10}px`,
+    });
+    gsap.to(cursorNext, 0.4, {
+      left: `${left + 60}px`,
+      top: `${top - 10}px`,
+    });
+  }
+}
+function scaleCursor(scale) {
+  gsap.to(cursor, 0.4, {
+    scale: scale,
+  });
+}
+
+card.addEventListener("mouseover", () => {
+  cursor.classList.add("more");
+  scaleCursor(5);
+});
+card.addEventListener("mouseleave", () => {
+  cursor.classList.remove("more");
+  scaleCursor(1);
+});
+
+list.addEventListener("mouseover", () => {
+  gsap.to(cursorPrev, 1, {
+    opacity: 1,
+  });
+  gsap.to(cursorNext, 1, {
+    opacity: 1,
+  });
+});
+list.addEventListener("mouseleave", () => {
+  gsap.to(cursorPrev, 1, {
+    opacity: 0,
+  });
+  gsap.to(cursorNext, 1, {
+    opacity: 0,
+  });
+});
+list.addEventListener("mousedown", (e) => {
+  isSlideOpen = true;
+  setCursorPosition(e.clientX, e.clientY);
+  scaleCursor(2);
+});
+list.addEventListener("mouseup", (e) => {
+  isSlideOpen = false;
+  setCursorPosition(e.clientX, e.clientY);
+  scaleCursor(1);
+});
+
+window.addEventListener("mousemove", (e) => {
+  if (isLoading) {
+    isLoading = false;
+    gsap.to(cursor, 0.4, {
+      opacity: 1,
+      delay: 0.2,
+    });
+  }
+  setCursorPosition(e.clientX, e.clientY);
+});
+window.addEventListener("mouseup", (e) => {
+  isSlideOpen = false;
+  setCursorPosition(e.clientX, e.clientY);
+  scaleCursor(1);
+});
