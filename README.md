cc04425b751981238fd3b0097739aa8dc34711b8
mizzu-creations <mizzu.creations@gmail.com>
2024-04-21 12:41:04 +0900

    Feat : GNB dropdown 영역 구현 - (2)

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # lotte-department-store-adaptive
 
-<0420 작업내용>
+<0421 작업내용>
 
-- noto sans kr 등록
-- nav 전체메뉴 만들기
+- nav 전체메뉴 마무리
 - dialog 처리
 - 공통 버튼 컴포넌트 만들기
--- js/gnbMenu.js
+++ js/gnbMenu.js
@@ -23,16 +23,36 @@ function updateContentHeight(idx) {
 }
 
 gnbMenu.addEventListener("mousemove", (e) => {
+  gnbMenuTabs.forEach((tab) => tab.classList.remove("--active"));
+  e.target.closest("li").classList.add("--active");
   currentIdx = gnbMenuTabs.indexOf(e.target.closest("li"));
   updateContentHeight(currentIdx);
 });
 
 gnbMenuContent.addEventListener("mouseleave", () => {
+  gnbMenuTabs.forEach((tab) => tab.classList.remove("--active"));
   gnbMenuContent.style.setProperty("--content-height", 0);
 });
 
 headerElements.forEach((element) => {
   element.addEventListener("mouseover", () => {
+    gnbMenuTabs.forEach((tab) => tab.classList.remove("--active"));
     gnbMenuContent.style.setProperty("--content-height", 0);
+  });
+});
+
+const subMenuTabs = document.querySelectorAll(".contents-list__tab > li span");
+const subMenuLists = document.querySelectorAll(".contents-list__menu");
+
+subMenuTabs.forEach((tab, i) => {
+  tab.addEventListener("mouseover", () => {
+    subMenuTabs.forEach((tab) => tab.classList.remove("--active"));
+    subMenuLists.forEach((tab) => tab.classList.remove("--active"));
+    tab.classList.add("--active");
+    subMenuLists[i].classList.add("--active");
+    gnbMenuContent.style.setProperty(
+      "--content-height",
+      subMenuLists[i].scrollHeight + "px"
+    );
   });
 });
