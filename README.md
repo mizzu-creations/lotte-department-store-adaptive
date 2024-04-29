39f1822397cc136d91c756532382fe48d629da1c
mizzu-creations <mizzu.creations@gmail.com>
2024-04-29 16:54:47 +0900

    Feat : main 영역 도입부 작업 (layer01)

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # lotte-department-store-adaptive
 
-- input select branchinfo 연동
 - 공통 버튼 컴포넌트 만들기
+- 스크롤바 적용
---
+++ js/layoutAnimationFlow.js
@@ -0,0 +1,74 @@
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+const startLayer = document.querySelector(".start-layer");
+const layer01 = document.querySelector(".enjoy-your-time");
+const layer01Bg = document.querySelector(".enjoy-your-time__wrap");
+const layer01Img = document.querySelector(".enjoy-your-time img");
+const scrollTopBtn = document.querySelector("#scroll-to-top");
+const text = new SplitType(".enjoy-your-time p");
+
+const tl = gsap.timeline({ defaults: { ease: "power3.inOut" } });
+
+tl.set(body, {
+  overflow: "hidden",
+})
+  .set(header, { y: -200, opacity: 0 })
+  .set(layer01, { backgroundColor: "#F8D873" })
+  .set(layer01Bg, { backgroundColor: "#F8D873" })
+  .set(text.chars, { y: 50, opacity: 0 })
+  .to(startLayer, {
+    yPercent: -100,
+    delay: 2,
+    duration: 1.5,
+  })
+  .fromTo(
+    scrollTopBtn,
+    { opacity: 0, scale: 1 },
+    { opacity: 1, duration: 0.5 },
+    "-=2.5"
+  )
+  .to(scrollTopBtn, { scale: 0, duration: 0.5 }, "-=1.5")
+  .fromTo(
+    layer01Img,
+    {
+      y: 100,
+      scale: 2,
+    },
+    {
+      y: -10,
+      duration: 2.5,
+    },
+    "-=2"
+  )
+  .to(layer01Img, {
+    y: 0,
+    scale: 1,
+    duration: 1.5,
+  })
+  .to(layer01, { backgroundColor: "#FFFFFF", duration: 1.5 }, "-=1.5")
+  .to(layer01Bg, { backgroundColor: "#FFFFFF", duration: 1.5 }, "<")
+  .to(header, { y: 0, opacity: 1, duration: 2 }, "-=1")
+  .to(
+    text.chars,
+    {
+      y: 0,
+      opacity: 1,
+      stagger: { each: 0.05 },
+      ease: "power3.inOut",
+      onComplete: () => {
+        body.style.overflow = "auto";
+      },
+    },
+    "-=1.5"
+  );
+
+tl.pause();
+
+window.addEventListener("DOMContentLoaded", () => {
+  window.scrollTo(0, 0);
+  tl.play();
+});
+window.addEventListener("resize", () => {
+  window.scrollTo(0, 0);
+  tl.restart();
+});
